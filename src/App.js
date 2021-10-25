928db6f17680af078d631016d11181836758abc1
zainuleb <p166024@nu.edu.pk>
2021-10-25 18:25:27 +0500

    Deploying to Heroku

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import React from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
@@ -9,7 +8,7 @@ import { selectCurrentUser } from "./redux/user/user.selector.js";
 import "./App.css";
 
 import HomePage from "./pages/homepage/homepage.component";
-import Header from './Components/header/header.component'
+import Header from "./Components/header/header.component.jsx";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Checkout from "./pages/checkout/Checkout";
